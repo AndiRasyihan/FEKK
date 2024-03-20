6097d2bdd8c5739185c8aa70cdcc752b2dd19356
AndiRasyihan <rasyihanjawahir@iqis.sch.id>
2024-03-20 09:09:34 +0800

    logo

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\Login;
+use App\Http\Controllers\Register;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/edit', function () {
+    return view('edit');
+});
+
+Route::get('/tabel', function () {
+    return view('tabel');
+});
+
+Route::get('/tambah', function () {
+    return view('tambah');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
